e90ef606e2ddc1926de2f8ad45b22503a301bd50
Varvara <k.vary@yandex.ru>
2025-04-26 18:43:39 +0300

    Add files via upload

---
+++ dog_configs/tags.py
@@ -0,0 +1,21 @@
+tags = {
+    'roads': {
+      "highway": ["construction","crossing","living_street","motorway","motorway_link","motorway_junction","pedestrian","primary","primary_link","raceway","residential","road","secondary","secondary_link","services","tertiary","tertiary_link","track","trunk","trunk_link","turning_circle","turning_loop","unclassified",],
+      "service": ["living_street", "emergency_access"]
+    },
+    'railways': {
+      "railway": "rail"
+    },
+    'water': {
+      'riverbank':True,
+      'reservoir':True,
+      'basin':True,
+      'dock':True,
+      'canal':True,
+      'pond':True,
+      'natural':['water','bay'],
+      'waterway':['river','canal','ditch'],
+      'landuse':'basin',
+      'water': 'lake'
+    }
+}
